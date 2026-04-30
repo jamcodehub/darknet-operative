df4148a38ac498a43ab6581b3fa413073f5ca11d
James <145176861+jamcodehub@users.noreply.github.com>
2026-04-30 15:55:17 +1000

    header padding height adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -237,7 +237,7 @@ function GhostDialogue({ dialogue, isGhostSpeaking, setIsGhostSpeaking }) {
         setDisplayedText(prev => prev + fullText[charIndex]);
         setCharIndex(charIndex + 1);
         setFrame(charIndex % 3 === 0 ? 'speaking' : 'idle');
-      }, 45);
+      }, 65);
       return () => clearTimeout(timer);
     }
   }, [charIndex, fullText]);
